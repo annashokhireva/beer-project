5c6b349c2a03c8e2e8a2e3a1662858f45d8b05a8
Anna Shokhireva <anna.shokhireva@iths.se>
2020-11-04 14:06:52 +0100

    progress

---
+++ main-js.js
@@ -0,0 +1,50 @@
+const searchParams = new URLSearchParams(window.location.search);
+const randomCard = document.querySelector('.random-card');
+const randomBtn = document.querySelector('button.random-btn')
+const api = 'https://api.punkapi.com/v2/beers';
+
+
+const removeAllChildNodes = (parent) => {
+
+    while (parent.firstChild) {
+
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function getData(url) {
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        beer = data[0];
+
+        getRandomBeer(beer);
+        
+    })
+    .catch(error => console.log(error));
+}
+
+randomBtn.addEventListener('click', onClick);
+
+function onClick(beer) {
+
+    removeAllChildNodes(randomCard);
+    const url = `${api}/random`;
+    
+    getData(url);  
+    
+}
+
+function getRandomBeer(beer){
+    let h2Tag = document.createElement('h2');
+
+    beerImg = new Image (54.4, 212.2);
+    beerImg.src = beer.image_url;
+
+    randomCard.appendChild(beerImg);
+
+    h2Tag.textContent = beer.name;
+    randomCard.appendChild(h2Tag);  
+}
