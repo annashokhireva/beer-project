0612a46af6833830aadda78cebfdbd6bd5f442e2
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2020-11-09 20:53:14 +0100

    nytt försök att pusha upp beer info

--- main-js.js
+++ main-js.js
@@ -26,16 +26,9 @@ function getData(url, callback) {
 
 randomBtn.addEventListener('click', onClick);
 
-<<<<<<< Updated upstream
-function onClick(beer) {
-    removeAllChildNodes(beerInfoDiv);
-    removeAllChildNodes(randomCard);
-    const url = `${api}/random`;
-=======
 function onClick(evt) {
 
    removeAllChildNodes(randomCard);
->>>>>>> Stashed changes
     
     const url = `${api}/random`;
     
@@ -54,86 +47,8 @@ function getRandomBeer(data){       //Skapar upp bild och namn på ranombeer
     randomCard.appendChild(beerImg);
     h2Tag.setAttribute('name', beer.id); 
     h2Tag.textContent = beer.name;
-<<<<<<< Updated upstream
-    randomCard.appendChild(h2Tag);   
-}
-
-//Beer Info start
-randomCard.addEventListener('click', beerInfo);
-    
-const beerInfoDiv = document.querySelector('div.beer-info');
-                
-function beerInfo() {
-    removeAllChildNodes(beerInfoDiv);
-    
-    let description = [
-        `${beer.description}`,
-        `Volume: ${beer.volume.value} ${beer.volume.unit}`,
-        `Alcohol by volume: ${beer.abv}`,
-        `Food pairing: ${beer.food_pairing}`,
-        `Brewers tips: ${beer.brewers_tips}`
-    ];
-
-
-    text = '<dl>';
-
-    for (let i = 0; i < description.length; i++) {
-        const descriptionInfo = description[i];
-        text += '<dt>' + descriptionInfo + '</dt>';
-    };
-
-    text += '</dl>';
-
-    let malt = beer.ingredients.malt;
-
-    maltText = '<dt> Malt:';
-
-    for (let i = 0; i < malt.length; i++){
-        let maltInfo = malt[i];
-        maltText += `<dd>${maltInfo.name} (${maltInfo.amount.value} 
-            ${maltInfo.amount.unit})</dd>`;
-    }
-
-    maltText += '</dt>'
-
-    let hops = beer.ingredients.hops;
-
-    hopsText = '<dt> Hops:';
-
-    for (let i = 0; i < hops.length; i++){
-        let hopsInfo = hops[i];
-        hopsText += `<dd>${hopsInfo.name} (${hopsInfo.amount.value} 
-            ${hopsInfo.amount.unit}): Add: ${hopsInfo.add} Attribute: ${hopsInfo.attribute}</dd>`;
-    }
-
-    hopsText += '</dt>'
-
-    yeastText = `<dt> Yeast: </dt> <dd>${beer.ingredients.yeast}</dd>`;
-    
-    const descriptionDiv = document.createElement('div');
-    const ingredientsDiv = document.createElement('div');
-    const maltP = document.createElement('p');
-    const hopsP = document.createElement('p');
-    const yeastP = document.createElement('p');
-    
-    beerInfoDiv.appendChild(beerImg);
-    beerInfoDiv.appendChild(h2Tag);
-    beerInfoDiv.appendChild(descriptionDiv);
-    descriptionDiv.innerHTML = text;
-    ingredientsDiv.innerText = 'Ingredients:';
-    descriptionDiv.appendChild(ingredientsDiv);
-    maltP.innerHTML = maltText;
-    ingredientsDiv.appendChild(maltP);
-    hopsP.innerHTML = hopsText;
-    ingredientsDiv.appendChild(hopsP)
-    yeastP.innerHTML = yeastText;
-    ingredientsDiv.appendChild(yeastP);
-    getRandomBeer(beer); //kommer ihåg vilket randomCard det var på sidan
-}   //beer Info End
-=======
     
     randomCard.appendChild(h2Tag);  
->>>>>>> Stashed changes
 
     h2Tag.addEventListener('click', beerInfo);
 }
