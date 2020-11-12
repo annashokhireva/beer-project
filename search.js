adb24d99bea692023d64528be2dd38173a55bcc2
Anna Shokhireva <anna.shokhireva@iths.se>
2020-11-12 09:18:53 +0100

    brewed before/after validation

--- search.js
+++ search.js
@@ -4,7 +4,6 @@ const beerList = document.querySelector('div.beer-list');
 const prevBtn = document.querySelector('div.prev-btn');
 const nextBtn = document.querySelector('div.next-btn');
 const goBack = document.querySelector('.go-back');
-
 const apiEndpoint = 'https://api.punkapi.com/v2/beers';
 const perPage = 10;
 let currentPage = 1;
@@ -20,10 +19,10 @@ function saveBeerList(data) {
 }
 
 
-formElement.addEventListener('submit', function(e) {
+formElement.addEventListener('submit', function (e) {
   e.preventDefault();
   removeAllChildNodes(beerList);
-  getData(render);  
+  getData(render);
   nextBtn.classList.remove('hidden');
   window.sessionStorage.removeItem('beersCash');
 });
@@ -33,15 +32,15 @@ prevBtn.addEventListener('click', function () {
 
   if (currentPage > 1) {
     currentPage--;
-    
-    if(window.sessionStorage.beersCash){
+
+    if (window.sessionStorage.beersCash) {
       let beersCash = window.sessionStorage.beersCash;
-      
+
       beersCash = JSON.parse(beersCash);
       console.log(beersCash);
 
-      if(beersCash['page' + currentPage]){
-      removeAllChildNodes(beerList);
+      if (beersCash['page' + currentPage]) {
+        removeAllChildNodes(beerList);
 
         render(beersCash['page' + currentPage]);
       }
@@ -53,10 +52,10 @@ prevBtn.addEventListener('click', function () {
 
       prevBtn.classList.remove('hidden');
       nextBtn.classList.remove('hidden');
-    }   
+    }
   }
 
-  if (currentPage <= 1){
+  if (currentPage <= 1) {
     prevBtn.classList.add('hidden');
   }
 });
@@ -66,30 +65,30 @@ nextBtn.addEventListener('click', function () {
   if (beers.length >= 10) {
     currentPage++;
 
-    if(window.sessionStorage.beersCash){
+    if (window.sessionStorage.beersCash) {
       let beersCash = window.sessionStorage.beersCash;
-      
+
       beersCash = JSON.parse(beersCash);
       console.log(beersCash);
       removeAllChildNodes(beerList);
-      if(beersCash['page' + currentPage]){      
+      if (beersCash['page' + currentPage]) {
 
         render(beersCash['page' + currentPage]);
       }
 
       else {
-        getData(render);    
+        getData(render);
       }
       nextBtn.classList.remove('hidden');
       prevBtn.classList.remove('hidden');
     }
-    
+
     else {
       getData(render);
 
       prevBtn.classList.remove('hidden');
-      nextBtn.classList.remove('hidden');  
-    } 
+      nextBtn.classList.remove('hidden');
+    }
   }
 });
 
@@ -102,24 +101,24 @@ function getQueryString(form) {
 
   let inputField = form.querySelectorAll('input');
 
-  for(input of inputField) {
+  for (input of inputField) {
     const value = input.value.trim();
 
-    if(value){
-      query.set(input.name, input.value);  
+    if (value) {
+      query.set(input.name, input.value);
     }
   }
-  
+
   return query.toString();
 }
 
 
-function removeAllChildNodes(parent){
+function removeAllChildNodes(parent) {
 
-    while (parent.firstChild) {
-  
-      parent.removeChild(parent.firstChild);
-    }
+  while (parent.firstChild) {
+
+    parent.removeChild(parent.firstChild);
+  }
 }
 
 
@@ -128,11 +127,11 @@ function getData(callback) {
   nextBtn.classList.add('hidden');
   url = apiEndpoint + '?' + getQueryString(formElement);
   console.log(url);
-  fetch(url, {cache: "force-cache"})
+  fetch(url, { cache: "force-cache" })
     .then(res => res.json())
     .then(data => {
       beers = data;
-      saveBeerList(data); 
+      saveBeerList(data);
       callback(beers);
     })
     .catch(error => console.log(error));
@@ -152,25 +151,25 @@ function render(beers) {
     liElement.setAttribute('name', beer.id);
     liElement.textContent = beer.name;
 
-    ulElement.appendChild(liElement); 
+    ulElement.appendChild(liElement);
   }
 
-  beerList.appendChild(ulElement); 
+  beerList.appendChild(ulElement);
 }
 
 
 function abvRanger() {
 
-  var parent = document.querySelector(".abv-slider");
+  let parent = document.querySelector(".abv-slider");
   if (!parent) return;
 
-  var
+  let
     rangeS = parent.querySelectorAll("input[type=range]"),
     numberS = parent.querySelectorAll("input[type=number]");
 
-  rangeS.forEach(function (el) {
-    el.oninput = function () {
-      var slide1 = parseFloat(rangeS[0].value),
+  rangeS.forEach(function (e) {
+    e.oninput = function () {
+      let slide1 = parseFloat(rangeS[0].value),
         slide2 = parseFloat(rangeS[1].value);
 
       if (slide1 > slide2) {
@@ -184,11 +183,11 @@ function abvRanger() {
 
   numberS.forEach(function (el) {
     el.oninput = function () {
-      var number1 = parseFloat(numberS[0].value),
+      let number1 = parseFloat(numberS[0].value),
         number2 = parseFloat(numberS[1].value);
 
       if (number1 > number2) {
-        var tmp = number1;
+        let tmp = number1;
         numberS[0].value = number2;
         numberS[1].value = tmp;
       }
@@ -202,17 +201,61 @@ function abvRanger() {
 abvRanger();
 
 
+function whenBrewed() {
+
+  let parent = document.querySelector(".test");
+  if (!parent) return;
+
+  let dateInputs = parent.querySelectorAll("input[type=text]");
+
+  dateInputs.forEach(function (e) {
+    e.oninput = function () {
+
+      let brewedAfter = dateInputs[0].value.split('-'),
+        brewedBefore = dateInputs[1].value.split('-');
+
+      for (let i = 0; i < brewedAfter.length; i++) {
+        let brewedAfterMonth = parseInt(brewedAfter[0], 10),
+          brewedAfterYear = parseInt(brewedAfter[1], 10);
+
+        for (let i = 0; i < brewedBefore.length; i++) {
+          let brewedBeforeMonth = parseInt(brewedBefore[0], 10),
+            brewedBeforeYear = parseInt(brewedBefore[1], 10);
+
+          if (brewedAfterYear > brewedBeforeYear) {
+            dateInputs[0].setCustomValidity("Invalid field.");
+            dateInputs[1].setCustomValidity("Invalid field.");
+          }
+
+          else if (brewedAfterYear === brewedBeforeYear & brewedAfterMonth > brewedBeforeMonth) {
+            dateInputs[0].setCustomValidity("Invalid field.");
+            dateInputs[1].setCustomValidity("Invalid field.");
+          }
+
+          else {
+            dateInputs[0].setCustomValidity("");
+            dateInputs[1].setCustomValidity("");
+          }
+        }
+      }
+
+    }
+  });
+}
+
+whenBrewed();
+
 goBack.addEventListener('click', onDiceClicked);
 
 function onDiceClicked() {
-    
-    const url = 'index.html';
-    document.location.href = url;
+
+  const url = 'index.html';
+  document.location.href = url;
 }
 
 
 function onUlClicked(e) {
-    
+
   const id = e.target.getAttribute('name');
   const url = `beerinfo.html?name=${id}`;
   document.location.href = url;
